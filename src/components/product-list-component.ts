2d8bfa2bbdc9dbbf4b78b67fdc2ed5a8def0f339
Leonablu <sergey.garbuzov.97@gmail.com>
2024-07-11 11:01:51 +0300

    Ready

--- src/components/product-list-component.ts
+++ src/components/product-list-component.ts
@@ -71,9 +71,9 @@ export class ProductListComponent extends ComponentWithModal<DisplayProductSetti
 		const categoryClassMap: { [key: string]: string } = {
 			'софт-скил': 'card__category_soft',
 			'хард-скил': 'card__category_hard',
-			другое: 'card__category_other',
-			дополнительное: 'card__category_additional',
-			кнопка: 'card__category_button',
+			'другое': 'card__category_other',
+			'дополнительное': 'card__category_additional',
+			'кнопка': 'card__category_button',
 		};
 
 		if (categoryClassMap[category]) {
